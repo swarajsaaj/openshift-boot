668ee643b1dee0cbdc7b04be0d832b516a37b68e
Swaraj Pal <swarajpalsaaj@yahoo.co.in>
2016-11-27 02:47:43 +0530

    Init commit

---
+++ src/main/java/io/github/swarajsaaj/SamplebootApplication.java
@@ -0,0 +1,12 @@
+package io.github.swarajsaaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplebootApplication.class, args);
+	}
+}
